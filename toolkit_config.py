ec61e94ebba13986a1ea6a8bc77ea8f171e23f77
HongfeiZhang11 <129837320+HongfeiZhang11@users.noreply.github.com>
2023-04-04 21:07:02 +1000

    Add files via upload

---
+++ toolkit_config.py
@@ -0,0 +1,11 @@
+"""toolkit_config.py
+
+Project configurarion file
+
+***do not subit this file***
+"""
+
+import os
+
+PRJDIR = r"C:\Users\Jerry\PycharmProjects\toolkit"
+DATADIR = os.path.join(PRJDIR, 'data')
